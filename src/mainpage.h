1a39b63fd570aafc69c9491872d9cccf8d1831dd
jrb <>
2002-03-19 21:21:17 +0000

    1. Implementation for new Util class 2. Add mainpage to bring facilities package up to snuff

---
+++ src/mainpage.h
@@ -0,0 +1,16 @@
+// Mainpage for doxygen
+
+/** @mainpage package facilities
+   <p> This package consists of simple utility classes.  One of them,
+       Util, is a place for static routines with no other logical home.
+    </p>
+
+<p>See release.notes for recent activity.</p>
+<hr>
+  @section requirements requirements
+  @verbinclude requirements
+
+  @todo Determine whether some of the classes in this package are obsolete.
+        If so, remove them.
+
+*/
