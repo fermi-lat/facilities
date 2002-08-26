48d22a2f468d6b1d89163683bae59532731fd990
jrb <>
2002-08-26 20:24:27 +0000

    General purpose timestamp class for dates in interval [1902, 2038] and delta times < 68 years.  Supported formats include Julian date, c run-time library (seconds from 1 jan 1970) and string.  Supported operations include comparisons, addition & subtraction where sensible.

---
+++ facilities/Timestamp.h
@@ -0,0 +1,170 @@
+// $Header: $
+#ifndef FACILITIES_TIMESTAMP_H
+#define FACILITIES_TIMESTAMP_H
+
+#include <string>
+
+namespace facilities {
+
+  /** Exception class, used to complain about bad arguments to 
+      constructors, incompatible arguments for comparisons or
+      arithmetic, or out of range results.
+  */
+  class BadTimeInput {
+  public:
+    BadTimeInput(std::string msg) : complaint(msg) {};
+    ~BadTimeInput() {};
+    std::string complaint; 
+
+    BadTimeInput(const BadTimeInput &other) {complaint = other.complaint; };
+  }; 
+
+
+  /** Timestamp class, valid for dates from 1902 through 2037 and for
+      non-negative delta times of up to about 68 years 
+
+      Supports comparisons, addition and subtraction where sensible
+
+      Input to constructors may be
+              Julian date      
+              seconds before or since start of 1970, Jan. 1 with optional
+                  nanosecond field
+              individual fields (year, month, etc.)
+              string format            
+
+           yyyy-mm-dd hh:mm:ss
+                             1901 < yyyy < 2038
+                          where 0 < mm < 13
+                                0 < dd < 32
+                               -1 < hh < 24
+                               -1 < mm < 60 
+                               -1 < ss < 60 
+
+        o  only the first three fields are required.  Omitted trailing
+           fields will be interpreted as equal to 0.
+        o  by default : will be used to delimit fields, but user
+           may specify an alternative in most circumstances
+        o  leading zeros are optional
+
+  */
+  class Timestamp {
+  public:
+    /// Default constructor builds object representing current time
+    Timestamp();
+
+    /// Count seconds from the creation-according-to-unix, start of 1970
+    Timestamp(long int seconds, int nano = 0, bool isDelta = false);
+
+    /// Constructor for Julian date 
+    Timestamp(double julian, bool isDelta = false);
+
+
+    /** Create a timestamp from an ascii string of standard form
+                yyyy-mm-dd hh:mm:ss
+        where only the first three fields are required.   
+
+        If the string is invalid, object will represent unix creation time.
+
+        Assume for now -- and preferably forever -- that all times are GMT.
+    */
+    Timestamp(const std::string& str);
+
+    /// Construct absolute time with specified fields
+    Timestamp(int year, int month, int day, 
+              int hour = 0, int minute = 0, 
+              int second = 0, int nano = 0);
+
+    /// Return string representation of time, not including nanoseconds;
+    /// valid for absolute times only
+    std::string getString() const;
+
+    /// Return julian date or, for delta times, just time interval
+    /// measured in days.
+    double      getJulian() const {
+      double julian = (m_time +  m_nano/inverseNano)/secPerDay;
+      if (!m_isDelta) julian += julian1970;
+      return julian;
+    }
+
+    double      getNano() const {return m_nano;}
+    long int    getClibTime() const {return m_time;}
+
+    bool        isDelta() const {return m_isDelta;}
+
+    bool operator<(const Timestamp& other) const {
+      if (m_isDelta != other.m_isDelta) 
+        throw BadTimeInput("facilities::Timestamp incompatible comparison");
+      return ((m_time < other.m_time) || 
+              ((m_time == other.m_time) && (m_nano < other.m_nano)));
+    }
+
+    bool operator>(const Timestamp& other) const {
+      return (other < (*this));
+        
+    }
+    Timestamp& operator= (const Timestamp& other) {
+      m_time = other.m_time; m_nano = other.m_nano; 
+      m_isDelta = other.m_isDelta; 
+      return *this;
+    }
+
+    bool operator==(const Timestamp& other) const{
+      if (m_isDelta != other.m_isDelta) 
+        throw BadTimeInput("facilities::Timestamp incompatible comparison");
+      return ((m_time == other.m_time) && (m_nano == other.m_nano));
+    }
+
+    bool operator!=(const Timestamp& other) const {
+      return (!(*this == other));
+    }
+
+    Timestamp&  operator+=(const Timestamp delta);
+
+    Timestamp&  operator-=(const Timestamp delta);
+
+    // To come:  operator+   operator- 
+    //             << (for debugging)
+  private:
+    // See the U.S. Naval Observatory site, 
+    //   http://aa.usno.navy.mil/data/docs/JulianDate.html,
+    // used to compute Julian date for Jan 1, 1970
+    static const double julian1970 = 2440587.5;
+    static const int    secPerDay =  (24*60*60);
+    static const double  inverseNano = 1000 * 1000 * 1000;
+    static const int     inverseNanoInt = 1000 * 1000 * 1000;
+    static const long int maxInt = 0x7fffffff;
+
+    /// Return "standard" binary time: count in seconds since 1 jan 1970
+    static time_t toBinary(const std::string& strTime); 
+
+    /// Assemble string rep. from count of seconds
+    static void toString(time_t bin, std::string& strTime);
+
+    /// internal binary rep of time; count seconds from Jan 1, 1970
+    time_t m_time;
+
+    /// Save fractional seconds separately (associated with m_time)
+    int  m_nano;
+
+    /// Flag to distinguish delta times from dates
+    bool m_isDelta;
+  };
+
+  /// Addition
+  static Timestamp operator+ (Timestamp a, 
+                              Timestamp b) {
+    Timestamp t = a;
+    return t += b;
+  }
+
+
+  /// Subtraction
+  static Timestamp operator- (Timestamp a, 
+                              Timestamp b) {
+    Timestamp t = a;
+    return t -= b;
+  }
+
+}
+
+#endif
---
+++ src/Timestamp.cxx
@@ -0,0 +1,244 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/calibUtil/src/Timestamp.cxx,v 1.4 2002/08/09 16:56:45 jrb Exp $
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include "facilities/Timestamp.h"
+
+namespace facilities {
+  // return current time (resolution of 1 second)
+  Timestamp::Timestamp() : m_nano(0) {
+    m_time = time(0);
+  }
+
+  // time specified as seconds since 1970
+  Timestamp::Timestamp(long int seconds, int nano, bool isDelta)
+    : m_time((time_t) seconds), m_nano(nano), m_isDelta(isDelta)
+  {
+    if  ((nano >= inverseNanoInt)  || (nano < 0))
+      throw BadTimeInput("facilities::Timestamp bad nano argument");
+  
+    if (m_isDelta) {
+      if (seconds < 0) 
+        throw BadTimeInput("facilities::Timestamp bad delta time");
+    }
+  }
+
+  // time specified as Julian date
+  Timestamp::Timestamp(double julian, bool isDelta) : m_isDelta(isDelta) {
+    double secs;
+    if (!isDelta) {
+      secs = (julian - julian1970) * secPerDay;
+
+      if ((fabs(secs) > maxInt)) 
+        throw BadTimeInput("Julian time not in range [1902, 2037]");
+    }
+    else {
+      secs = julian * secPerDay;
+      if ((secs > maxInt)  || (secs < 0) )
+        throw BadTimeInput("Julian delta time negative or too large");
+      m_time = (long int) secs;
+      //      m_strTime.clear();
+    }
+    m_time = (long int) secs;
+
+    // In case time is negative, truncation will go the "wrong way".
+    // Want m_time always <= secs
+    if (m_time > secs) m_time--;
+    m_nano = (int) ((secs - m_time)/inverseNano);
+  } 
+
+  // Time specified as string
+  Timestamp::Timestamp(const std::string& str) : m_nano(0), m_isDelta(false){
+    m_time = toBinary(str);
+  }
+
+  // Time specified with independent fields
+  Timestamp::Timestamp(int year, int month, 
+                       int day, int hour, 
+                       int minute, int second,
+                       int nano) :  m_nano(nano), m_isDelta(false) {
+    struct tm fields;
+
+    // check input   
+    // for now don't bother checking whether, e.g., someone
+    // specified April 31
+    if ((month < 1 ) || (month > 12) || (day < 1) || (day > 31) ||
+        (hour < 0) || (hour > 23) || 
+        (minute < 0) || (minute > 59) || 
+        (second < 0 ) || (second >= 60) ||
+        (year < 1902) || (year > 2037) || 
+        (nano < 0 ) || (nano >= inverseNanoInt) ) 
+      throw BadTimeInput("facilities::Timestamp Bad subfield");
+
+    fields.tm_year = year - 1900;
+    fields.tm_mon = month - 1;
+    fields.tm_mday = day;
+    fields.tm_hour = hour;
+    fields.tm_min = minute;
+    fields.tm_sec = (long int) second;
+    fields.tm_wday = -1;
+    fields.tm_yday = -1;
+    
+    fields.tm_isdst = 0;   // daylight savings time?
+
+    m_time = timegm(&fields);
+  }
+
+  std::string Timestamp::getString() const {
+    std::string str;
+
+    toString(m_time, str);
+    return str;
+  }
+
+
+  time_t Timestamp::toBinary(const std::string& strTime) {
+    // Break out fields
+    struct tm fields;
+
+    unsigned int pos;
+    unsigned int oldPos = 0;
+
+    // Three delimiter characters may occur in ascii time representation.
+    // First is hyphen.
+    char  delim = '-';
+
+    pos = strTime.find(delim, oldPos);
+
+    // Must have two occurrences of hyphen delimiter
+    if (pos >= strTime.size()) return 0;
+
+    fields.tm_year = atoi((strTime.substr(oldPos, pos)).c_str()) - 1900;
+    if ((fields.tm_year < 2) || (fields.tm_year > 137)) 
+      throw BadTimeInput("facilities::Timestamp  bad year");
+    oldPos = pos + 1;
+    pos = strTime.find(delim, oldPos);
+
+    // Should have at least two occurrences of delim
+    if (pos >= strTime.size()) 
+      throw BadTimeInput("Bad string format for facilities::Timestamp");
+
+    fields.tm_mon = atoi((strTime.substr(oldPos, pos)).c_str()) - 1;
+    if ((fields.tm_mon < 0) || (fields.tm_mon > 11)) 
+      throw BadTimeInput("facilities::Timestamp bad month");
+
+    // day
+    oldPos = pos + 1;
+
+    // A space separates time from date (if time is present at all)
+    delim = ' ';
+    pos = strTime.find(delim, oldPos);
+
+    fields.tm_mday = atoi((strTime.substr(oldPos, pos)).c_str());
+
+    if ((fields.tm_mday < 1) || (fields.tm_mday > 31))
+    throw BadTimeInput("facilities::Timestamp bad day of month");
+
+    // Remaining fields in string representation default to 0.
+    fields.tm_hour = fields.tm_min = fields.tm_sec = 0;
+
+    if (pos < strTime.size() ) {   // more fields to process
+      delim = ':';
+      oldPos = pos + 1;
+
+      pos = strTime.find(delim, oldPos);
+
+      fields.tm_hour = atoi((strTime.substr(oldPos, pos)).c_str());
+      if ((fields.tm_hour > 23) || (fields.tm_hour < 0)) 
+      throw BadTimeInput("facilities::Timestamp bad hour");
+
+      
+      if (pos < strTime.size() ) {
+        oldPos = pos + 1;
+        pos = strTime.find(delim, oldPos);
+        fields.tm_min = atoi((strTime.substr(oldPos, pos)).c_str());
+        if ((fields.tm_min > 59) || (fields.tm_hour < 0))
+          throw BadTimeInput("facilities::Timestamp bad minutes");
+
+        if (pos < strTime.size() ) {
+          oldPos = pos + 1;
+          pos = strTime.find(delim, oldPos);
+          fields.tm_sec = atoi((strTime.substr(oldPos, pos)).c_str());
+          if ((fields.tm_sec > 59) || (fields.tm_hour < 0)) 
+            throw BadTimeInput("facilities::Timestamp bad seconds");
+        }
+      }
+    }
+
+    fields.tm_wday = -1;
+    fields.tm_yday = -1;
+    fields.tm_isdst = 0;
+    return timegm(&fields);
+  }
+  
+  void Timestamp::toString(time_t bin, std::string& strTime) {
+    struct tm * fields = gmtime(&bin);
+
+    strTime.resize(0);
+    
+    char buf[20];
+    char* bufPtr = &buf[0];
+    sprintf(buf, "%i", fields->tm_year + 1900);
+    strTime += bufPtr; strTime +=  "-";
+    sprintf(buf, "%02i", fields->tm_mon +1);
+    strTime += bufPtr; strTime +=  "-";
+    sprintf(buf, "%02i", fields->tm_mday);
+    strTime += bufPtr; strTime +=  " ";
+    sprintf(buf, "%02i", fields->tm_hour);
+    strTime += bufPtr; strTime +=  ":";
+    sprintf(buf, "%02i", fields->tm_min);
+    strTime += bufPtr; strTime +=  ":";
+    sprintf(buf, "%02i", fields->tm_sec);
+    strTime += bufPtr;
+  }
+
+  // Arithmetic.  Can only add/subtract delta time
+
+  Timestamp& Timestamp::operator+=(const Timestamp delta) {
+    if (!delta.isDelta()) 
+      throw BadTimeInput("facilities::Timestamp need delta time");
+    
+    double sum = m_time + delta.m_time;
+    if (sum > maxInt) 
+      throw BadTimeInput("facilities::Timestamp result out of range");
+
+    m_time += delta.m_time;
+    m_nano += delta.m_nano;
+    if (m_nano >= inverseNanoInt) {
+      if (m_time == maxInt) {
+        throw BadTimeInput("facilities::Timestamp out of range");
+      }
+      m_time++;
+      m_nano -= inverseNanoInt;
+    }
+    return *this;
+  }
+
+  Timestamp& Timestamp::operator-=(const Timestamp delta) {
+    if (!delta.isDelta()) 
+      throw BadTimeInput("facilities::Timestamp need delta time");
+
+    if (isDelta())  {
+      if (*this < delta) // result is delta. No negative deltas allowed
+        throw BadTimeInput("facilities::Timestamp result out of range");
+    }
+    else {         // absolute time minus a delta
+      double diff = m_time - delta.m_time;
+      if (-diff > maxInt) {
+        throw BadTimeInput("facilities::Timestamp out of range");
+      }
+      if ((-diff == maxInt) && (m_nano < delta.m_nano)) {
+        throw BadTimeInput("facilities::Timestamp out of range");
+      }
+    }
+    m_time -= delta.m_time;
+    m_nano -= delta.m_nano;
+    if (m_nano < 0) {
+      m_time--;
+      m_nano += inverseNanoInt;
+    }
+    return *this;
+  }
+}
