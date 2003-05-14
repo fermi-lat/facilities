6979fc288d08b921c338d2ff491a546c961c7da8
jrb <>
2003-05-14 16:25:57 +0000

    Use try.. catch around calls to Util::expandEnvVar

--- src/test/testUtil.cxx
+++ src/test/testUtil.cxx
@@ -10,18 +10,29 @@ int main(int, char**) {
   std::string name = std::string("{FACILITIESROOT}/src");
   std::string oDelim = std::string ("{");
   std::string cDelim = std::string ("}");
+  int ntrans;
 
-  int ntrans = facilities::Util::expandEnvVar(&name, oDelim, cDelim);
+  try {
+    ntrans = facilities::Util::expandEnvVar(&name, oDelim, cDelim);
+    std::cout << "Translated name is " << name << std::endl;  }
+  catch (facilities::Untranslatable err) {
+    std::cout << "Failed to translate " << err.badVar << std::endl;
+  }
 
-  std::cout << "Translated name is " << name << std::endl;
+
 
   // Process running this must have environment variable SRC
   std::string multi = std::string("$(FACILITIESROOT)/$(SRC)");
 
-  ntrans = facilities::Util::expandEnvVar(&multi);
+  try {
+    ntrans = facilities::Util::expandEnvVar(&multi);
 
-  std::cout << "Translated name is " << multi << std::endl;
-  std::cout << ntrans << " variables were translated." << std::endl;
+    std::cout << "Translated name is " << multi << std::endl;
+    std::cout << ntrans << " variables were translated." << std::endl;
+  }
+  catch (facilities::Untranslatable err) {
+    std::cout << "Failed to translate " << err.badVar << std::endl;
+  }
 
   // Test the new itoa routine
   std::string intStr;
