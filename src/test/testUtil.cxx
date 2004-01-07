c94cb73cfc111f564e62bbd5014de1e0fcf05557
jrb <>
2004-01-07 01:12:04 +0000

    Enhance test program to invoke new utilities

--- src/test/testUtil.cxx
+++ src/test/testUtil.cxx
@@ -38,6 +38,32 @@ int main(int, char**) {
   std::string intStr;
   facilities::Util::itoa(12, intStr);
   std::cout << "My String is " << intStr << std::endl;
+
+  // Test stringTokenize and basename
+  std::string unixname("/a/path/myUnixFile.txt");
+  std::string wname("C:\\a\\longer\\path\\myWindowsFile.txt");
+
+  std::vector<std::string> tokens;
+  unsigned int i;
+
+  facilities::Util::stringTokenize(unixname, "/", tokens);
+
+  std::cout << "Processing string " << unixname << std::endl;
+  for (i = 0; i < tokens.size(); i++) {
+    std::cout << "Token " << i << " is: " << tokens[i] << std::endl;
+  }
+
+  std::cout << "basename is " << facilities::Util::basename(unixname) << std::endl;
+
+  facilities::Util::stringTokenize(wname, "\\", tokens);
+
+  std::cout << "Processing string " << wname << std::endl;
+  for (i = 0; i < tokens.size(); i++) {
+    std::cout << "Token " << i << " is: " << tokens[i] << std::endl;
+  }
+
+  std::cout << "basename is " << facilities::Util::basename(wname) << std::endl;
+
   return 0;
 }
 
