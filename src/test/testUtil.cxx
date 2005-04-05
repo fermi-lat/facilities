52d8b12c98154e9d2061c193488b2902334b38ba
jrb <>
2005-04-05 06:01:15 +0000

    requirements:  add back in test_time application Timestamp.h, Timestamp.cxx: Add optional time zone offset arguments to                             some constructors Util.h, Util.cxx   Rename stringTokenize (new map-filling version) to                   keyValueTokenize and revise slightly test_time.cxx, testUtil.cxx: exercise new stuff

--- src/test/testUtil.cxx
+++ src/test/testUtil.cxx
@@ -13,6 +13,7 @@ int main(int, char**) {
   std::string cDelim = std::string ("}");
   int ntrans;
 
+  std::cout << "Test expandEnvVar: " << std::endl;
   try {
     ntrans = facilities::Util::expandEnvVar(&name, oDelim, cDelim);
     std::cout << "Translated name (right delimiters) is " 
@@ -40,18 +41,20 @@ int main(int, char**) {
     std::cout << "Failed to completely translate " << multi << std::endl;
   }
 
+  std::cout << std::endl << "Test itoa " << std::endl;
   // Test the new itoa routine
   std::string intStr;
   facilities::Util::itoa(12, intStr);
   std::cout << "My String is " << intStr << std::endl;
 
-  // Test stringTokenize and basename
+  // basename & stringTokenize
   std::string unixname("/a/path/myUnixFile.txt");
   std::string wname("C:\\a\\longer\\path\\myWindowsFile.txt");
 
   std::vector<std::string> tokens;
   unsigned int i;
 
+  std::cout << std::endl << "Test stringTokenize and basename" << std::endl;
   facilities::Util::stringTokenize(unixname, "/", tokens);
 
   std::cout << "Processing string " << unixname << std::endl;
@@ -70,10 +73,12 @@ int main(int, char**) {
 
   std::cout << "basename is " << facilities::Util::basename(wname) << std::endl;
 
-  //test stringTokenize to map routine
+  //test keyValueTokenize to map routine
+  std::cout << std::endl << "Test keyValueTokenize " << std::endl;
   std::string input1("apple=green,lemon=yellow,blueberry=blue");
+  std::cout << "Input string: '" << input1 << "'" << std::endl;
   std::map<std::string,std::string> maptokens;
-  facilities::Util::stringTokenize(input1,",","=",maptokens,false);
+  facilities::Util::keyValueTokenize(input1,",",maptokens,"=", false);
   std::map<std::string,std::string>::const_iterator tokens_itr=maptokens.begin();
   while (tokens_itr!=maptokens.end()) {
     std::cout << "Token key " <<(*tokens_itr).first  << " and value: " 
@@ -83,7 +88,8 @@ int main(int, char**) {
 
   std::cout <<"appending to previous map:"<<std::endl;
   std::string input2("apple2/green2,lemon2/yellow2,blueberry2/blue2");
-  facilities::Util::stringTokenize(input2,",","/",maptokens,false);
+  std::cout << "New string is '" << input2 << "'" << std::endl;
+  facilities::Util::keyValueTokenize(input2,",",maptokens,"/",false);
   tokens_itr=maptokens.begin();
   while (tokens_itr!=maptokens.end()) {
     std::cout << "Token key " <<(*tokens_itr).first  << " and value: " 
@@ -92,7 +98,18 @@ int main(int, char**) {
   }
   
   std::cout <<"clearing the map first:"<<std::endl;
-  facilities::Util::stringTokenize(input2,",","/",maptokens,true);
+  facilities::Util::keyValueTokenize(input2,",",maptokens,"/",true);
+  tokens_itr=maptokens.begin();
+  while (tokens_itr!=maptokens.end()) {
+    std::cout << "Token key " <<(*tokens_itr).first  << " and value: " 
+	      << (*tokens_itr).second << std::endl;
+    tokens_itr++;
+  }
+
+  std::cout << "Use a multi-character pairDelimiter argument " << std::endl;
+  std::string input3("apple2:=green2 lemon2:=yellow2 blueberry2:=blue2");
+  std::cout << "input is: '" << input3 << "'" << std::endl;
+  facilities::Util::keyValueTokenize(input3," ",maptokens,":=");
   tokens_itr=maptokens.begin();
   while (tokens_itr!=maptokens.end()) {
     std::cout << "Token key " <<(*tokens_itr).first  << " and value: " 
@@ -102,6 +119,7 @@ int main(int, char**) {
 
 
   // Test stringToDouble routine
+  std::cout << std::endl << "Test stringToDouble " << std::endl;
   std::string okDouble("3.14159");
   std::string badDouble("3.garbage56");
 
@@ -128,6 +146,8 @@ int main(int, char**) {
   }
 
   // Test stringToInt routine
+  std::cout << std::endl << "Test stringToInt " << std::endl;
+
   std::string okInt("33550");
   std::string badInt1("3garbage56");
   std::string badInt2("garbage356");
@@ -165,6 +185,7 @@ int main(int, char**) {
   }
 
   // Try out trimTrailing method
+  std::cout << std::endl << "Test trimTrailing " << std::endl;
   std::string string1("ends with 2 blanks  ");
   std::string string2("ends with newline\n");
   std::string string3("no trailing whitespace");
