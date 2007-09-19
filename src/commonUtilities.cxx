ef438c205e7c94479da02ea06745adf46e3bff72
jrb <>
2007-09-19 01:02:39 +0000

    1. Put the class in namespace facilities 2. Do things assuming CMT file hierarchy for now

--- src/commonUtilities.cxx
+++ src/commonUtilities.cxx
@@ -2,73 +2,77 @@
 #include <iostream>
 #include <algorithm>
 
-std::string commonUtilities::getPackagePath(const std::string &package){
-  return commonUtilities::getPackageRoot(package);
-}
-
-std::string commonUtilities::getDataPath(const std::string &package){
-  std::string packageRoot = commonUtilities::getPackageRoot(package);
-  if(packageRoot=="")
-    return packageRoot;
-#ifdef WIN32
-  return packageRoot+"\\data\\"+package;
-#else
-  return packageRoot+"/data/"+package;
-#endif
-}
-
-std::string commonUtilities::getXmlPath(const std::string &package){
-  std::string packageRoot = commonUtilities::getPackageRoot(package);
-  if(packageRoot=="")
-    return packageRoot;
-#ifdef WIN32
-  return packageRoot+"\\xml\\"+package;
-#else
-  return packageRoot+"/xml/"+package;
-#endif
-}
-
-std::string commonUtilities::getPfilesPath(const std::string &package){
-  std::string packageRoot = commonUtilities::getPackageRoot(package);
-  if(packageRoot=="")
-    return packageRoot;
-#ifdef WIN32
-  return packageRoot+"\\pfiles\\"+package;
-#else
-  return packageRoot+"/pfiles/"+package;
-#endif
-}
-
-void commonUtilities::setEnvironment(const std::string &name, const std::string &value, bool overwrite){
-  if(getenv(name.c_str())==NULL || overwrite){
-    setenv(name.c_str(), value.c_str(),1);
+namespace facilities {
+  std::string commonUtilities::getPackagePath(const std::string &package){
+    return commonUtilities::getPackageRoot(package);
   }
-}
 
-std::string commonUtilities::getEnvironment(const std::string &name){
-  const char *env = getenv(name.c_str());
-  std::string toReturn;
-  if(env!=NULL)
-    toReturn = env;
-  return toReturn;
-}
+  std::string commonUtilities::getDataPath(const std::string &package){
+    std::string packageRoot = commonUtilities::getPackageRoot(package);
+    if(packageRoot=="")
+      return packageRoot;
+#ifdef WIN32
+    return packageRoot+"\\data";
+#else
+    return packageRoot+"/data";
+#endif
+  }
 
-std::string commonUtilities::getPackageRoot(const std::string &package){
-  std::string upperCase=package;
-  transform(upperCase.begin(),upperCase.end(),upperCase.begin(),(int(*)(int)) toupper);
-  upperCase=upperCase+"ROOT";
-  const char *env = getenv(upperCase.c_str());
-  std::string packageRoot;
-  if(env!=NULL)
-    packageRoot = env;
-  if(packageRoot == ""){
-    env = getenv("INST_DIR");
+  std::string commonUtilities::getXmlPath(const std::string &package){
+    std::string packageRoot = commonUtilities::getPackageRoot(package);
+    if(packageRoot=="")
+      return packageRoot;
+#ifdef WIN32
+    return packageRoot+"\\xml";
+#else
+    return packageRoot+"/xml";
+#endif
+  }
+
+  std::string commonUtilities::getPfilesPath(const std::string &package){
+    std::string packageRoot = commonUtilities::getPackageRoot(package);
+    if(packageRoot=="")
+      return packageRoot;
+#ifdef WIN32
+    return packageRoot+"\\pfiles";
+#else
+    return packageRoot+"/pfiles";
+#endif
+  }
+
+  void commonUtilities::setEnvironment(const std::string &name, const std::string &value, bool overwrite){
+    if(getenv(name.c_str())==NULL || overwrite){
+      setenv(name.c_str(), value.c_str(),1);
+    }
+  }
+
+  std::string commonUtilities::getEnvironment(const std::string &name){
+    const char *env = getenv(name.c_str());
+    std::string toReturn;
+    if(env!=NULL)
+      toReturn = env;
+    return toReturn;
+  }
+
+  std::string commonUtilities::getPackageRoot(const std::string &package){
+    std::string upperCase=package;
+    transform(upperCase.begin(),upperCase.end(),upperCase.begin(),(int(*)(int)) toupper);
+    upperCase=upperCase+"ROOT";
+    const char *env = getenv(upperCase.c_str());
+    std::string packageRoot;
     if(env!=NULL)
       packageRoot = env;
+    //  For now insist there be a translation for package root
+    /*
+      if(packageRoot == ""){
+      env = getenv("INST_DIR");
+      if(env!=NULL)
+        packageRoot = env;
+      }
+    */
+    if(packageRoot == ""){
+      std::cerr<<"Unable to determine data path for "<<package<<std::endl;
+    }
+    return packageRoot;
   }
-  if(packageRoot == ""){
-    std::cerr<<"Unable to determine data path for "<<package<<std::endl;
-  }
-  return packageRoot;
 }
-
