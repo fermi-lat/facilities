756d188a9d592efa30c2ceb023f3a440f8b4abf0
jrb <>
2007-09-19 01:02:06 +0000

    Put the class in namespace facilities

--- facilities/commonUtilities.h
+++ facilities/commonUtilities.h
@@ -2,17 +2,18 @@
 #define _COMMON_UTILITIES_H_
 
 #include <string>
-
-class commonUtilities{
- public:
-  static std::string getDataPath(const std::string &package);
-  static std::string getXmlPath(const std::string &package);
-  static std::string getPfilesPath(const std::string &package);
-  static std::string getPackagePath(const std::string &package);
-  static void setEnvironment(const std::string &name, const std::string &value, bool overwrite=false);
-  static std::string getEnvironment(const std::string &name);
- private:
-  static std::string getPackageRoot(const std::string &package);
-};
+namespace facilities {
+  class commonUtilities{
+  public:
+    static std::string getDataPath(const std::string &package);
+    static std::string getXmlPath(const std::string &package);
+    static std::string getPfilesPath(const std::string &package);
+    static std::string getPackagePath(const std::string &package);
+    static void setEnvironment(const std::string &name, const std::string &value, bool overwrite=false);
+    static std::string getEnvironment(const std::string &name);
+  private:
+    static std::string getPackageRoot(const std::string &package);
+  };
+}
 
 #endif
