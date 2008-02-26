22a5fbc878b85b1a96c4d86b81f1634ebac0bc9e
glastrm <>
2008-02-26 03:15:14 +0000

    Set lib dependencies up for when building libraries

--- facilitiesLib.py
+++ facilitiesLib.py
@@ -1,6 +1,7 @@
-#$Id$
+#$Id: facilitiesLib.py,v 1.2 2008/02/22 01:30:26 golpa Exp $
 def generate(env, **kw):
-	env.Tool('addLibrary', library = ['facilities'], package = 'facilities')
+	if not kw.get('depsOnly',0):
+		env.Tool('addLibrary', library = ['facilities'])
 
 def exists(env):
 	return 1;
