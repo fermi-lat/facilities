ae09b10fa70b6ab66d5b3a86b3d33e039721f74b
cvs2git <>
2008-04-03 19:20:54 +0000

    This commit was manufactured by cvs2svn to create tag 'v2r18p11'.
    Sprout from master 2008-04-03 19:20:53 UTC heather 'add expandEnvVarList'
    Delete:
        .cvsignore
        ChangeLog
        SConscript
        cmt/.cvsignore
        cmt/requirements
        doc/index.html
        doc/release.notes
        facilitiesLib.py
        python/facilities.py
        src/Clock.cxx
        src/ScheduledEvent.cxx
        src/Scheduler.cxx
        src/Timestamp.cxx
        src/Util.cxx
        src/XGetopt.cxx
        src/commonUtilities.cxx
        src/mainpage.h
        src/py_facilities.in
        src/test/testUtil.cxx
        src/test/test_env.cxx
        src/test/test_time.cxx

--- facilitiesLib.py
+++
@@ -1,7 +0,0 @@
-#$Id: facilitiesLib.py,v 1.2 2008/02/22 01:30:26 golpa Exp $
-def generate(env, **kw):
-	if not kw.get('depsOnly',0):
-		env.Tool('addLibrary', library = ['facilities'])
-
-def exists(env):
-	return 1;
