ab15eee13fc1b0106114e58f2d03af6360cc8523
Tom Stephens <thomas.stephens@gmail.com>
2019-12-06 11:28:13 -0700

    Fixed tabs and indention issues

--- facilitiesLib.py
+++ facilitiesLib.py
@@ -2,9 +2,8 @@
 
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
-	env.Tool('addLibrary', library = ['facilities'])
-	if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','')=='GlastRelease':
-
-	    env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('addLibrary', library = ['facilities'])
+    if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'facilities') 
 def exists(env):
     return 1;
